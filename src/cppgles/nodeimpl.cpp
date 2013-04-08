ea842cf56f4add09c32013838cf570e191630116
Josh Marinacci <joshua@marinacci.org>
2013-04-08 13:23:19 -0700

    got rect really working in node

--- src/cppgles/nodeimpl.cpp
+++ src/cppgles/nodeimpl.cpp
@@ -11,34 +11,6 @@ using namespace v8;
 
 class GLGFX: public GFX, public node::ObjectWrap {
 public:
-    static void Init() {
-        Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
-        tpl->SetClassName(String::NewSymbol("GLGFX"));
-        tpl->InstanceTemplate()->SetInternalFieldCount(1);
-        tpl->PrototypeTemplate()->Set(String::NewSymbol("fillQuadColor"),FunctionTemplate::New(node_fillQuadColor)->GetFunction());
-        constructor = Persistent<Function>::New(tpl->GetFunction());
-    }
-    
-    static Handle<v8::Value> New(const v8::Arguments& args) {
-        HandleScope scope;
-        GLGFX* self  = new GLGFX();
-        printf("inside New. trans = %f\n",self->transform[0]);
-        printf("New: instance = %d\n",self);
-        self->Wrap(args.This());
-        return scope.Close(args.This());
-        //return args.This();
-    }
-
-    static Handle<v8::Value> NewInstance(const v8::Arguments& args) {
-        HandleScope scope;
-        Handle<Value> argv[] = {args[0]};
-        Local<Object> instance = constructor->NewInstance(1, argv);
-        GLGFX* self = ObjectWrap::Unwrap<GLGFX>(instance);
-        printf("inside NewInstance. trans = %f\n",self->transform[0]);
-        printf("NewInstance: instance = %d\n",self);
-        return scope.Close(instance);
-    }
-    
     static void dumpValue(Local<Value> val) {
         if(val.IsEmpty()) { printf("is empty\n"); }
         if(val->IsFunction()) { printf("it is a function\n"); }
@@ -58,48 +30,33 @@ public:
         if(val->IsRegExp()) {  printf("it is a Reg Exp\n");  }
     }
     static Handle<v8::Value> node_fillQuadColor(const v8::Arguments& args) {
-        //printf("inside GLGFX.node_fillQuadColor\n");
         HandleScope scope;
         GLGFX* self = ObjectWrap::Unwrap<GLGFX>(args.This());
-        printf("=======\n");
         Local<Value> arg(args[0]);
         
         v8::String::Utf8Value param1(args[0]->ToString());
         std::string foo = std::string(*param1);    
-        printf("str %s\n",foo.c_str());
-        if(args[0]->IsString()) {
-            printf("arg 0 is a string object\n");  
-        }
+        //printf("str %s\n",foo.c_str());
         
         if(args[1]->IsObject()) {
-//            printf("arg 1 is an object\n");
             Local<Object> bnds = args[1]->ToObject();
             Local<Value> w = bnds->Get(String::New("w"));
-            dumpValue(w);
-//            double dw = w->NumberValue();
+            //dumpValue(w);
             double dx = bnds->Get(String::New("x"))->NumberValue();
             double dy = bnds->Get(String::New("y"))->NumberValue();
             double dw = bnds->Get(String::New("w"))->NumberValue();
             double dh = bnds->Get(String::New("h"))->NumberValue();
-//            printf("width = %f\n",dw);
-//            printf("height = %f\n",dh);
             self->fillQuadColor(NULL,new BBounds(dx,dy,dw,dh));
         }
 
-        int n= 1;
-        //dumpValue(args[n]);
-        printf("here\n");
-        //self->fillQuadColor(NULL,new BBounds(0,0,100,100));
         return scope.Close(Undefined());
     }
     
     GLfloat* transform;
     stack<void*> matrixStack;
     GLGFX() {
-        printf("GLGFX constructor called\n");
         transform = new GLfloat[16];
         make_identity_matrix(transform);
-        printf("trans set to %f\n",transform[0]);
     }
     ~GLGFX() {
     }
@@ -130,7 +87,7 @@ public:
         float y =  bounds->getY();
         float x2 = bounds->getX()+bounds->getW();
         float y2 = bounds->getY()+bounds->getH();
-        printf("filling quad color with %f,%f -> %f,%f\n",x,y,x2,y2);
+        //printf("filling quad color with %f,%f -> %f,%f\n",x,y,x2,y2);
     
         
         GLfloat verts[6][2];
@@ -145,24 +102,6 @@ public:
             }
         }
         
-        /*
-        static GLfloat sverts[6][2] = {
-          { -1, -1 },
-          {  1, -1 },
-          {  1,  1 },
-          {  1,  1 },
-          { -1,  1 },
-          { -1, -1 }
-        };
-        */
-        
-        /*
-        for(int i=0; i<6; i++) {
-            for(int j=0; j<2; j++) {
-                verts[i][j] = sverts[i][j];
-            }
-        }
-        */
         verts[0][0] = x;
         verts[0][1] = y;
         verts[1][0] = x2;
@@ -177,7 +116,7 @@ public:
         verts[5][0] = x;
         verts[5][1] = y;
         
-        printf("view = %f, trans = %f\n", modelView[0], transform[0]);
+        //printf("view = %f, trans = %f\n", modelView[0], transform[0]);
         colorShader->apply(modelView, transform,verts,colors);
     }
     void scale(double x, double y){
@@ -185,21 +124,8 @@ public:
     void rotate(double theta){
     }
 private:
-    static v8::Persistent<v8::Function> constructor;
 };
-Persistent<Function> GLGFX::constructor;
 
-
-/*
-    void draw(GFX* gfx) {
-        printf("NodeRect::draw\n");
-        Bounds* bounds = getBounds();
-        GLGFX* g = (GLGFX*)gfx;
-        setFill(new BColor(0.5,0.6,0.7));
-        g->fillQuadColor(getFill(), bounds);
-    }
-*/
-    
 class NodeCore : public Core , public node::ObjectWrap {
 public:
     static void Init() {
@@ -238,7 +164,6 @@ public:
     ~NodeCore() { }
     
     static v8::Handle<v8::Value> real_OpenWindow(const v8::Arguments& args) {
-        printf("wrappered NodeCore:CreateStage\n");
         HandleScope scope;
         int ret = glfwOpenWindow(640, 480, 8, 8, 8, 0, 24, 0, GLFW_WINDOW);
         if(!ret) {
@@ -253,16 +178,10 @@ public:
     
     static v8::Handle<v8::Value> real_Start(const v8::Arguments& args) {
         HandleScope scope;
-//        NodeCore* self = Unwrap<NodeCore>(args.This());
-        printf("real_Start\n");
-        
         colorShader = new ColorShader();
-        printf("calling GLFW\n");
         Local<Function> rootCB = Local<Function>::Cast(args[0]);        
-//        printf("checking root\n");
         while (glfwGetWindowParam(GLFW_OPENED))
         {
-            printf("main drawing loop\n");
             glClear( GL_COLOR_BUFFER_BIT );
             
             GLfloat rot[16], scale[16], trans[16];
@@ -281,7 +200,6 @@ public:
             
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             
-            //https://developers.google.com/v8/embed
             Handle<ObjectTemplate> point_templ = ObjectTemplate::New();
             point_templ->SetInternalFieldCount(1);
             point_templ->Set(String::NewSymbol("fillQuadColor"),FunctionTemplate::New(GLGFX::node_fillQuadColor)->GetFunction());
@@ -310,7 +228,6 @@ Persistent<Function> NodeCore::constructor;
 
 
 Handle<Value> CreateObject(const Arguments& args) {
-    printf("invoking CreateObject\n");
     HandleScope scope;
     return scope.Close(NodeCore::NewInstance(args));
 }
@@ -318,7 +235,6 @@ Handle<Value> CreateObject(const Arguments& args) {
 
 void InitAll(Handle<Object> exports, Handle<Object> module) {
   NodeCore::Init();
-  GLGFX::Init();
   module->Set(String::NewSymbol("exports"),FunctionTemplate::New(CreateObject)->GetFunction());
 }
 

c11eef89ed8f784f7bd487c2c3df04b3b50e03a7
Josh Marinacci <joshua@marinacci.org>
2013-04-24 09:30:22 -0700

    cleanup

--- tests/flickrtest.js
+++ tests/flickrtest.js
@@ -6,7 +6,6 @@ var feedr = new Feedr();
 
 var feeds = {
     flickr: {
-        //url: "https://github.com/bevry/feedr/commits/master.atom"
         url:"http://api.flickr.com/services/feeds/photos_public.gne?id=31706743@N00&lang=en-us&format=rss_200"
     }
 };
@@ -17,7 +16,6 @@ feedr.readFeeds(feeds, function(err, result) {
     for(var i=0; i<data.length; i++) {
         var item = data[i]
         var thumb = item["media:thumbnail"][0]["$"];
-//        console.log(thumb);
         console.log("photo " + item.title /* + " at " + item.link*/ + " with thumb: ", thumb.url + " size = " + thumb.width + "x" + thumb.height);
         thumbs.push(thumb);
     }
@@ -25,10 +23,7 @@ feedr.readFeeds(feeds, function(err, result) {
 
 var core = amino.getCore();
 core.setDevice("mac");
-
 var stage = core.createStage();
-
-
 var root = core.createGroup();
 stage.setRoot(root);
 
@@ -39,32 +34,29 @@ grid.DEBUG = true;
 grid.cellHeight = 75;
 grid.cellWidth = 75;
 grid.layout = "horizwrap";
-
 grid.listModel = thumbs;
-
-root.add(grid);
-
-function createThumbTexture(th) {
-    stage.loadRemoteTexture(th.url, th.width, th.height, function(id) {
-        th.texid = id;
-    });
-}
-
-var setupTextures = function() {
-    for(var i=0; i<thumbs.length; i++) {
-        var th = thumbs[i];
-        if(!th) continue;
-        console.log(th);
-        createThumbTexture(th);
-    }
-}
-
 grid.cellRenderer = function(gfx, item, bounds) {
     gfx.fillQuadColor(new amino.Color(0.5,0.5,0.5), bounds);
     if(item.texid) {
         gfx.fillQuadTexture(item.texid,bounds.x,bounds.y,75,75);
     }
 }
+root.add(grid);
+
+
+var setupTextures = function() {
+    function createThumbTexture(th) {
+        stage.loadRemoteTexture(th.url, th.width, th.height, function(id) {
+            th.texid = id;
+        });
+    }
+    for(var i=0; i<thumbs.length; i++) {
+        var th = thumbs[i];
+        if(!th) continue;
+        createThumbTexture(th);
+    }
+}
+
 
 setTimeout(function(){
     core.start();

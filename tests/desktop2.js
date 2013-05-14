42b861e8380897b4aab3bc46f9d417130d11ac6f
Josh Marinacci <joshua@marinacci.org>
2013-05-14 14:59:41 -0700

    updated desktop test

--- tests/desktop2.js
+++ tests/desktop2.js
@@ -35,8 +35,161 @@ function setupWeather() {
     });
 }
 
+function setupMusic() {
+        var lib = {
+            albums: [
+                {
+                    name:"Yellow Submarine",
+                    artist: "The Beatles",
+                    artwork: "tests/images/beatles_03.jpg",
+                    tracks: [
+                        {
+                            title: "Yellow Submarine",
+                        },
+                        {
+                            title: "Hey Bulldog",
+                        },
+                        {
+                            title: "Eleanor Rigby",
+                        },
+                        {
+                            title: "Nowhere Man",
+                        }
+                    ]
+                },
+                {
+                    name:"Abbey Road",
+                    artist: "The Beatles",
+                    artwork: "tests/images/beatles_01.jpg",
+                    tracks: [
+                        {
+                            title:"Come Together"
+                        },
+                        {
+                            title: "Something"
+                        },
+                        {
+                            title: "Maxwell's Silver Hammer",
+                        }
+                    ]
+                },
+                {
+                    name:"For Sale",
+                    artist: "The Beatles",
+                    artwork: "tests/images/beatles_02.jpg",
+                    tracks: [
+                        {
+                            title:"No Reply"
+                        },
+                        {
+                            title: "I'm A Loser"
+                        },
+                        {
+                            title: "Baby's In Black",
+                        }
+                    ]
+                },
+                {
+                    name:"Hopes and Fears",
+                    artist: "Keane",
+                    artwork: "tests/images/keane_01.jpg",
+                    tracks: [
+                        {
+                            title:"Somewhere Only We Know"
+                        },
+                        {
+                            title: "This Is The Last Time"
+                        },
+                        {
+                            title: "Bend & Break",
+                        }
+                    ]
+                },
+                {
+                    name:"Perfect Symmetry",
+                    artist: "Keane",
+                    artwork: "tests/images/keane_02.jpg",
+                    tracks: [
+                        {
+                            title:"Spiralling"
+                        },
+                        {
+                            title: "The Lovers Are Losing"
+                        },
+                        {
+                            title: "Better Than This",
+                        }
+                    ]
+                }
+            ]
+        }
+    var currentAlbum = lib.albums[0];
+    var currentTrack = 0;
+    
+    var title = stage.find("musicTitle");
+    function prevTrack() {
+        currentTrack--;
+        if(currentTrack < 0) currentTrack = 0;
+        title.setText(currentAlbum.tracks[currentTrack].title);
+    }
+    stage.on("ACTION",stage.find("prevButton"),prevTrack);
+    function nextTrack() {
+        currentTrack++;
+        if(currentTrack > currentAlbum.tracks.length-1) {
+            currentTrack = 0;
+        }
+        title.setText(currentAlbum.tracks[currentTrack].title);
+    }
+    stage.on("ACTION",stage.find("nextButton"),nextTrack);
+    
+    
+    var artwork = stage.find("musicArtwork");
+    artwork.sw = 75;
+    artwork.sh = 75;
+    function setAlbum(album) {
+        currentAlbum = album;
+        currentTrack = 0;
+        var url = currentAlbum.artwork;
+        console.log(url);
+        artwork.setUrl(url);
+        title.setText(currentAlbum.tracks[currentTrack].title);
+    }
+    setAlbum(lib.albums[0]);
+    
+    var popup = stage.find("musicPopup");
+    popup.setTx(0);
+    popup.setTy(0);
+    popup.setVisible(false);
+    
+    var albumList = stage.find("albumList");
+    albumList.listModel = lib.albums;
+    albumList.cellRenderer = function(gfx, info, bounds) {
+        var color = amino.ParseRGBString("#ccffff");//amino.Color(0.5,0.5,0.5);
+        if(info.list.selectedIndex == info.index) {
+            color = new amino.Color(0.1,0.7,1.0);
+        }
+        gfx.fillQuadColor(color, bounds);
+        
+        gfx.fillQuadText(new amino.Color(0,0,0),
+            info.item.name,
+            bounds.x+5, bounds.y, info.list.getFontSize(), info.list.font.fontid);
+    };
+    stage.on("SELECT",albumList, function(e) {
+        var n = e.index;
+        console.log("selected index: " + n);
+        setAlbum(lib.albums[n]);
+    });
+    stage.on("ACTION",stage.find("musicOpenButton"), function() {
+            popup.setVisible(true);
+    });
+    stage.on("ACTION",stage.find("musicCloseButton"), function() {
+            popup.setVisible(false);
+    });
+}
+
 setupClock();
 setupWeather();
+setupMusic();
 
 setTimeout(function() {
     core.start();

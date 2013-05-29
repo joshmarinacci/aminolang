1e21406b4d8539a80e1ec511c0d71c3d6a425d50
Josh Marinacci <joshua@marinacci.org>
2013-05-28 18:21:03 -0700

    updates to fix segfault

--- src/node/klaatu.cpp
+++ src/node/klaatu.cpp
@@ -1,12 +1,21 @@
 #include <node.h>
 #include <math.h>
 #include <time.h>
+#include <ui/DisplayInfo.h>
+#include <ui/FramebufferNativeWindow.h>
+#include <gui/SurfaceComposerClient.h>
+
+#define ASSERT_EQ(A, B) {if ((A) != (B)) {printf ("ERROR: %d\n", __LINE__); exit(9); }}
+#define ASSERT_NE(A, B) {if ((A) == (B)) {printf ("ERROR: %d\n", __LINE__); exit(9); }}
+#define EXPECT_TRUE(A) {if ((A) == 0) {printf ("ERROR: %d\n", __LINE__); exit(9); }}
 
 
-#include "core.h"
-
+using namespace v8;
+using namespace node;
 using android::sp;
 
+
+static GLfloat* modelView;
 static EGLDisplay mEglDisplay = EGL_NO_DISPLAY;
 static EGLSurface mEglSurface = EGL_NO_SURFACE;
 static EGLContext mEglContext = EGL_NO_CONTEXT;
@@ -18,6 +27,50 @@ static int audioCount = 0;
 static double elapsedTime;
 static int elapsedCount;
 
+static void
+make_identity_matrix(GLfloat *m) {
+   int i;
+   for (i = 0; i < 16; i++)
+      m[i] = 0.0;
+    m[0] = 1;
+    m[5] = 1;
+    m[10] = 1;
+    m[15] = 1;
+}
+
+static void
+mul_matrix(GLfloat *prod, const GLfloat *a, const GLfloat *b)
+{
+#define A(row,col)  a[(col<<2)+row]
+#define B(row,col)  b[(col<<2)+row]
+#define P(row,col)  p[(col<<2)+row]
+   GLfloat p[16];
+   GLint i;
+   for (i = 0; i < 4; i++) {
+      const GLfloat ai0=A(i,0),  ai1=A(i,1),  ai2=A(i,2),  ai3=A(i,3);
+      P(i,0) = ai0 * B(0,0) + ai1 * B(1,0) + ai2 * B(2,0) + ai3 * B(3,0);
+      P(i,1) = ai0 * B(0,1) + ai1 * B(1,1) + ai2 * B(2,1) + ai3 * B(3,1);
+      P(i,2) = ai0 * B(0,2) + ai1 * B(1,2) + ai2 * B(2,2) + ai3 * B(3,2);
+      P(i,3) = ai0 * B(0,3) + ai1 * B(1,3) + ai2 * B(2,3) + ai3 * B(3,3);
+   }
+   memcpy(prod, p, sizeof(p));
+#undef A
+#undef B
+#undef PROD
+}
+
+static void
+loadPixelPerfect(GLfloat *m, float width, float height, float z_eye, float z_near, float z_far) {
+    float kdn = z_eye - z_near;
+    float kdf = z_eye - z_far;
+    float ksz = - (kdf+kdn)/(kdf-kdn);
+    float ktz = - (2.0f*kdn*kdf)/(kdf-kdn);
+    
+    m[0]=(2.0f*z_eye)/width; m[1]=0;                    m[2]=0; m[3]=0;
+    m[4]=0;                  m[5]=(2.0f*z_eye)/height;  m[6]=0; m[7]=0;
+    m[8]=0; m[9]=0; m[10]=ktz-ksz*z_eye; m[11]=-1.0f;
+    m[12]=0; m[13]=0; m[14]=ktz; m[15]=z_eye;
+}
 void klaatu_init_graphics(int *width, int *height)
 {
   
@@ -77,11 +130,94 @@ void klaatu_init_graphics(int *width, int *height)
 
 static int winWidth;
 static int winHeight;
-class KlaatuCore : public NodeCore , public node::ObjectWrap{
+GLint u_matrix, u_trans;
+GLint attr_pos;
+GLint attr_color;    
+int prog;
+
+int compileVertShader(const char* text) {
+    GLint stat;
+    GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertShader, 1, (const char **) &text, NULL);
+    glCompileShader(vertShader);
+    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &stat);
+    if (!stat) {
+        printf("Error: vertex shader did not compile!\n");
+    }
+    return vertShader;
+}
+
+int compileFragShader(const char* text) {
+    GLint stat;
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragShader, 1, (const char **) &text, NULL);
+    glCompileShader(fragShader);
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &stat);
+    if (!stat) {
+        printf("Error: fragment shader did not compile!\n");
+    }
+    return fragShader;
+}
+
+int compileProgram(int vertShader, int fragShader) {
+    GLuint program = glCreateProgram();
+    GLint stat;
+    glAttachShader(program, vertShader);
+    glAttachShader(program, fragShader);
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &stat);
+    if (!stat) {
+        char log[1000];
+        GLsizei len;
+        glGetProgramInfoLog(program, 1000, &len, log);
+        printf("Error: linking:\n%s\n", log);
+    }
+    return program;
+}
+
+void load_color_shader() {
+       static const char *fragShaderText =
+      "precision mediump float;\n"
+      "varying vec4 v_color;\n"
+      "void main() {\n"
+      "   gl_FragColor = v_color;\n"
+      "}\n";
+      
+   static const char *vertShaderText =
+      "uniform mat4 modelviewProjection;\n"
+      "uniform mat4 trans;\n"
+      "attribute vec4 pos;\n"
+      "attribute vec4 color;\n"
+      "varying vec4 v_color;\n"
+      "void main() {\n"
+      "   gl_Position = trans * pos * modelviewProjection;\n"
+      "   v_color = color;\n"
+      "}\n";
+
+   GLuint vert = compileVertShader(vertShaderText);
+   GLuint frag = compileFragShader(fragShaderText);
+   prog = compileProgram(vert,frag);
+   
+   glUseProgram(prog);
+   attr_pos   = glGetAttribLocation(prog, "pos");
+   attr_color = glGetAttribLocation(prog, "color");
+   u_matrix   = glGetUniformLocation(prog, "modelviewProjection");
+   u_trans    = glGetUniformLocation(prog, "trans");
+
+}
+
+class KlaatuCore : public node::ObjectWrap{
 public:
+    KlaatuCore() {
+    }
+    ~KlaatuCore() { }
+    static v8::Persistent<v8::Function> constructor;
     virtual void start() {
         printf("started the KlaatuCore\n");
     }
+    
+    
+    
     static v8::Handle<v8::Value> real_Init(const v8::Arguments& args) {
         HandleScope scope;
         printf("doing real Klaatu init\n");
@@ -93,10 +229,14 @@ public:
             printf("Error: eglGetDisplay() failed\n");
         }
         glClearColor(1.0, 1.0, 1.0, 1.0);
-        colorShader = new ColorShader();
+        
+        
+        //colorShader = new ColorShader();
+        load_color_shader();
+        
         modelView = new GLfloat[16];
         printf("=== about to create a memory leak\n");
-        void* leak2 = malloc(5000);
+        void* leak2 = malloc(10*1000);
         printf("=== allocated 10k of memory\n");
         printf("finishing up with init\n");
         
@@ -111,12 +251,6 @@ public:
         printf("doing mul matrix\n");
         mul_matrix(modelView, proj, idmat);
         printf("creating a new instance\n");
-        
-        Handle<Value> obj = GLGFX::NewInstance(args);
-        GLGFX* gfx = node::ObjectWrap::Unwrap<GLGFX>(obj->ToObject());        
-        gfx->scale(1,-1);
-        gfx->translate(-winWidth/2,-winHeight/2);
-        
         printf("about to swap buffers\n");
         
         eglSwapBuffers(mEglDisplay, mEglSurface);
@@ -131,8 +265,6 @@ public:
     }
     
     static void Init() {
-        elapsedTime = 0;
-        elapsedCount = 0;
         Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
         tpl->SetClassName(String::NewSymbol("Core"));
         tpl->InstanceTemplate()->SetInternalFieldCount(1);
@@ -160,6 +292,7 @@ public:
     
     
 };
+Persistent<Function> KlaatuCore::constructor;
 
 Handle<Value> CreateObject(const Arguments& args) {
     HandleScope scope;
@@ -168,11 +301,9 @@ Handle<Value> CreateObject(const Arguments& args) {
 
 void InitAll(Handle<Object> exports, Handle<Object> module) {
     KlaatuCore::Init();
-    GLGFX::Init();
+    //GLGFX::Init();
     exports->Set(String::NewSymbol("createCore"),FunctionTemplate::New(CreateObject)->GetFunction());
 }
-
-
 
 NODE_MODULE(aminonative, InitAll)
 

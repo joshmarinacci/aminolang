6574baca2d3156e4889ad36249e0e18abb8297a5
Josh Marinacci <joshua@marinacci.org>
2013-06-07 14:00:07 -0700

    added results searching

--- tests/desktop/search.js
+++ tests/desktop/search.js
@@ -31,9 +31,11 @@ function setupContacts(nav,stage) {
             var con = contents[i];
             if(con.firstname.toLowerCase().indexOf(txt) !== -1) {
                 results.push(con);
+                continue;
             }
             if(con.lastname.toLowerCase().indexOf(txt)  !== -1) {
                 results.push(con);
+                continue;
             }
         }
         return results;
@@ -54,12 +56,78 @@ function setupContacts(nav,stage) {
         }
         view.setFontSize(15);
     }
+    function hideResults() {
+        popup.setVisible(false);
+    }
+    /*
     stage.on("CHANGED",cl,function(e) {
         var results = searchContacts(cl.getText());
         if(results.length > 0) {
             showResults(results);
         }
     });
+    */
+    
+    var actions = [
+        {
+            regex: /^s*call\s+/i,
+            name:"call",
+            complete: function(text) {
+                var name = text.match(/^s*call\s+(.*)$/i);
+                if(name) {
+                    console.log("auto completing with the text:",name[1]);
+                    var results = searchContacts(name[1]);
+                    console.log(results);
+                    if(results.length > 0) {
+                        showResults(results);
+                    } else {
+                        hideResults();
+                    }
+                }
+            }
+        },
+        {
+            regex: /^s*todo\s+/i,
+            name:"todo",
+        },
+        {
+            regex: /^s*e?mail\s+/i,
+            name:"mail"
+        }
+    ];
+    
+    var currentAction = null;
+    function applyAction(text) {
+        console.log("applying",text, " to action",currentAction);
+    }
+    
+    function evaluateAction(text) {
+        console.log("evaluating",text);
+        for(var i in actions) {
+            var act = actions[i];
+            if(text.match(act.regex)) {
+                currentAction = actions[i];
+                console.log("evaluating action: " + act.name);
+                if(act.complete) {
+                    act.complete(text);
+                }
+                return;
+            }
+        }
+        currentAction = null;
+        hideResults();
+    }
+    
+    stage.on("KEYPRESS",cl,function(kp) {
+            console.log("pressed: ",kp.keycode);
+            //pressed the enter key
+        if(kp.keycode == 294) {
+            applyAction(cl.getText());
+        } else {
+            evaluateAction(cl.getText());
+        }
+    });
+
 }
 
 exports.setupContacts = setupContacts;

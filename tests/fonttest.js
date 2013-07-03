1f42dc740ba90e1c9ac3cea6d4c457c93b085f1c
Joshua Marinacci <joshua.marinacci@nokia.com>
2013-07-03 12:23:52 -0700

    deleted files we don't need anymore

--- tests/fonttest.js
+++
@@ -1,9 +0,0 @@
-var amino = require('../build/desktop/amino.js');
-var core = amino.getCore();
-core.setDevice("mac");
-var stage = core.createStage();
-
-var button = core.createPushButton().setText("ABC");
-stage.setRoot(button);
-core.start();
-

9ba5841267be5cf4288ac984167be0af8de6247b
Joshua Marinacci <joshua.marinacci@nokia.com>
2013-09-04 16:45:58 -0700

    remove or comment dead code

--- src/sg/amino.js
+++ src/sg/amino.js
@@ -581,6 +581,7 @@ exports.ProtoText = exports.ComposeObject({
 });
 
 
+/** Base of all UI controls like buttons and labels */
 exports.ProtoWidget = exports.ComposeObject({
     type: "widget",
     comps: {
@@ -618,187 +619,10 @@ exports.ProtoWidget = exports.ComposeObject({
     }
 });
 
-/** SGNode class. Base of all nodes */
-function SGNode() {
-	this.live = false;
-	
-	this.createProp = function(name,handle) {
-		this["set"+camelize(name)] = function(v) {
-			this[name] = v;
-			if(this.live) {
-			    if(!propsHash[name]) {
-			        console.log("WARNING: no prop key for " + name + " " + this.id);
-			    }
-				sgtest.updateProperty(handle, propsHash[name],v);
-				if(this.propertyUpdated) {
-				    this.propertyUpdated(name,v);
-				}
-			}
-			return this;
-		};
-		this["get"+camelize(name)] = function() {
-			return this[name];
-		};
-	}
-	
-	this.setProp = function(handle, name, value) {
-		if(handle == null)  console.log("WARNING can't set prop " + name + ", the handle is null");
-		sgtest.updateProperty(handle, propsHash[name],value);
-	}
-
-    
-	this.delegateProps = function(props, handle) {
-		for(var name in props) {
-			this[name] = props[name]; //set the initial value
-			this.createProp(name,handle);
-			sgtest.updateProperty(handle, propsHash[name], props[name]);
-		}
-	}
-	
-	this.id = "noid";
-	this.setId = function(id) {
-		this.id = id;
-		return this;
-	}
-	this.getId = function() {
-	    return this.id;
-	}
-	this.getParent = function() {
-	    return this.parent;
-	}
-	this.visible = true;
-    this.setVisible = function(visible) {
-        this.visible = visible;
-        this.setProp(this.handle,'visible',visible?1:0);            
-        return this;
-    }	
-    this.getVisible = function() {
-        return this.visible;
-    }
-}
-
-/** SGRect class. A simple rectangle with a fill color. No border. */
-function SGRect() {
-	SGNode(this);
-    this.init = function() {
-        this.handle = sgtest.createRect();
-        console.log("==== handle = " + this.handle);
-        this.live = true;
-        var props = { tx:0, ty:0, x:0, y:0, w:100, h:100, r: 0, g: 1, b:0, scalex: 1, scaley:1, rotateZ: 0, rotateX:0, rotateY: 0, visible:1 };
-        this.delegateProps(props,this.handle);
-        this.setFill = function(color) {
-            color = ParseRGBString(color);
-            this.setProp(this.handle,'r',color.r);
-            this.setProp(this.handle,'g',color.g);
-            this.setProp(this.handle,'b',color.b);
-            return this;
-        }
-        this.getFill = function() {
-            return this.color;
-        }
-    }
-    this.contains = function(x,y) {
-        if(x >=  this.getX()  && x <= this.getX() + this.getW()) {
-            if(y >= this.getY() && y <= this.getY() + this.getH()) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-SGRect.extend(SGNode);
-
-/**
- * A Group class
- */
-function SGGroup() {
-    
-    this.live = false;
-    this.children = [];
-    this.add = function(node) {
-    	if(!node) abort("can't add a null child to a group");
-        if(!this.live) abort("error. trying to add child to a group that isn't live yet");
-        this.children.push(node);
-        node.parent = this;
-        sgtest.addNodeToGroup(node.handle,this.handle);
-    }
-    this.isParent = function() { return true; }
-    this.getChildCount = function() {
-    	return this.children.length;
-    }
-    this.getChild = function(i) {
-    	return this.children[i];
-    }
-    this.remove = function(target) {
-        var n = this.children.indexOf(target);
-        this.children.splice(n,1);
-        target.parent = null;
-    }
-    this.clear = function() {
-        for(var i in this.children) {
-            this.children[i].setVisible(false);
-        }
-        this.children = [];
-    }
-            
-    
-    this.init = function() {
-        this.handle = sgtest.createGroup();
-        var props = { tx:0, ty:0, scalex:1, scaley:1, rotateX:0, rotateY:0, rotateZ:0, visible:1};
-        this.delegateProps(props,this.handle);
-        this.live = true;
-    }
-}
-SGGroup.extend(SGNode);
-
-
-
-/** Base of all UI controls like buttons and labels */
-function SGWidget() {
-    this.contains = function(x,y) {
-        if(x >=  this.getX()  && x <= this.getX() + this.getW()) {
-            if(y >= this.getY() && y <= this.getY() + this.getH()) {
-                return true;
-            }
-        }
-        return false;
-    }
-    this.propertyUpdated = function() {
-    }
-    this.createLocalProp = function(name,value) {
-    	this["set"+camelize(name)]= function(v) {
-    		this[name] = v;
-    		this.propertyUpdated(name,v);
-    		return this;
-    	}
-		this["get"+camelize(name)] = function() {
-			return this[name];
-		};
-		this["set"+camelize(name)](value);
-    }
-    this.createLocalProps = function(props) {
-		for(var name in props) {
-			this.createLocalProp(name,props[name]);
-		}
-    }
-    
-    this.createLocalProps({
-    	anchorLeft:true,
-    	anchorRight:false,
-    	anchorTop:true,
-    	anchorBottom:false,
-    	left:0,
-		right:0,
-		top:0,
-		bottom:0,
-    });
-}
-SGWidget.extend(SGNode);
-
-
 
 /** AnchorPanel is a container which lays out it's children using anchor constraints like
 top and left */
+/*
 function SGAnchorPanel() {
     this.live = false;
     this.children = [];
@@ -871,9 +695,10 @@ function SGAnchorPanel() {
     }
 }
 SGAnchorPanel.extend(SGWidget);
-
+*/
 
 /** A basic label */
+/*
 function SGLabel() {
     this.live = false;
     this.init = function() {    
@@ -898,9 +723,10 @@ function SGLabel() {
     }
 }
 SGLabel.extend(SGWidget);
-
+*/
 
 /** A spinner to indicate progress of some activity */
+/*
 function SGSpinner() {
 	this.active = false;
 	this.init = function(core) {
@@ -952,7 +778,7 @@ function SGSpinner() {
     this.contains = function() { return false; }
 }
 SGSpinner.extend(SGWidget);
-
+*/
 /** A list view. Very complex */
 /*
 function SGListView() {
@@ -1083,6 +909,7 @@ SGListView.extend(SGWidget);
 */
 
 /** ImageView: a widget to show an image. Can scale it. */
+/*
 function SGImageView() {
     this.init = function(core) {
         this.handle = sgtest.createRect();
@@ -1107,6 +934,7 @@ function SGImageView() {
     }
 }
 SGImageView.extend(SGWidget);
+*/
 /*
 function SGTextControl() {
     textcontrol.JSTextControl.call(this);
@@ -1319,84 +1147,6 @@ the callback
 * @class
 */
 function Core() {
-
-    /** create a test node */
-	this.createTestNode = function() {
-		var node = new SGTestNode();
-		node.init();
-		return node;
-	}
-	
-	/** Create a rect */
-    this.createRect = function() {
-        var node = new SGRect();
-        node.init();
-        return node;
-    };
-    /** create a label */
-    this.createLabel = function() {
-        var node = new SGLabel();
-        node.font = this.font;
-        node.init();
-        return node;
-    }
-    this.createGroup = function() {
-        var node = new SGGroup();
-        node.init();
-        return node;
-    }
-    
-    this.createPushButton = function() {
-        var node = new SGButton();
-        node.font = this.defaultFont;
-        node.init(this);
-        return node;
-    }
-    this.createToggleButton = function() {
-        var node = new SGToggleButton();
-        node.font = this.defaultFont;
-        node.init(this);
-        return node;
-    }
-    this.createSlider = function() {
-    	var node = new SGSlider();
-    	node.init(this);
-    	return node;
-    }
-    this.createSpinner = function() {
-    	var node = new SGSpinner();
-    	node.init(this);
-    	return node;
-    }
-    this.createAnchorPanel = function() {
-    	var node = new SGAnchorPanel();
-		node.init(this);
-		return node;
-    }
-    this.createListView = function() {
-    	var node = new SGListView();
-    	node.init(this);
-    	return node;
-    }
-    this.createImageView = function() {
-        var node = new SGImageView();
-        node.init(this);
-        return node;
-    }
-    
-    this.createTextField = function() {
-        var node = new SGTextField();
-        node.font = this.defaultFont;
-        node.init(this);
-        return node;
-    }
-    this.createTextArea = function() {
-        var node = new SGTextArea();
-        node.font = this.defaultFont;
-        node.init(this);
-        return node;
-    }
-    
     this.anims = [];
     this.createPropAnim = function(node, prop, start, end, dur, count, autoreverse) {
         var anim = new SGAnim(node,prop,start,end,dur,count,autoreverse);

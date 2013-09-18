fb9555bb2fdc8b60f87f4fec8aa4c69baecfd9a1
Josh Marinacci <joshua@marinacci.org>
2013-09-18 11:41:58 -0700

    added moment class

--- tests/phone/lockscreen.js
+++ tests/phone/lockscreen.js
@@ -1,5 +1,6 @@
 var amino = null;
 var widgets = null;
+var moment = require('moment');
 if(process.platform == 'darwin') {
     amino = require('../../build/desktop/amino.js');
     widgets = require('../../build/desktop/widgets.js');
@@ -16,13 +17,30 @@ function LockScreen(core,stage) {
         g.setW(e.width/2).setH(e.height/2);
     });
 
-
-    g.add(new widgets.Label().setText("12:38").setTx(10)
+    console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    var timeLabel = new widgets.Label()
+        .setText("12:38")
+        .setTx(10)
         .setFill("#ffffff")
         .setFontSize(80)
-        .setAnchorTop(true).setTop(20)
-        );
-    g.add(new amino.ProtoText().setText("Tuesday Jan 18th").setTx(20).setTy(100).setFill("#ffffff").setFontSize(20));
+        .setAnchorTop(true).setTop(20);
+    g.add(timeLabel);
+    var dateLabel = new amino.ProtoText().setText("Tuesday Jan 18th").setTx(20).setTy(100).setFill("#ffffff").setFontSize(20);
+    g.add(dateLabel);
+    
+    var weatherLabel = new widgets.Label()
+        .setText("80deg, sunny")
+        .setTx(20)
+        .setFill("#ffffff")
+        .setFontSize(25)
+        .setAnchorTop(true).setTop(160);
+    g.add(weatherLabel);
+    
+    setInterval(function() {
+        timeLabel.setText(moment().format('h:mm'));
+        dateLabel.setText(moment().format('dddd MMM Do'));
+    },1000);
+    
     //time
     //date
     //weather
@@ -30,8 +48,8 @@ function LockScreen(core,stage) {
     
     
     g.add(new widgets.PushButton().setText("unlock")
-        .setLeft(20).setAnchorLeft(true)
-        .setRight(20).setAnchorRight(true)
+        .setLeft(10).setAnchorLeft(true)
+        .setRight(10).setAnchorRight(true)
         .setBottom(10).setAnchorBottom(true)
         .setW(200).setH(40).onAction(function() {
                 g.setVisible(false);

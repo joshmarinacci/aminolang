2ee0167aa1d9ab2c178a36588791bfbd5dfd8145
Joshua Marinacci <joshua.marinacci@nokia.com>
2013-09-18 19:42:32 -0700

    added drag

---
+++ tests/perf/drag1.js
@@ -0,0 +1,21 @@
+var amino = null;
+var widgets = null;
+if(process.platform == 'darwin') {
+    amino = require('../../build/desktop/amino.js');
+    widgets = require('../../build/desktop/widgets.js');
+} else {
+    amino = require('./amino.js');    
+    widgets = require('./widgets.js');
+}
+
+
+amino.startApp(function(core, stage) {
+    stage.setSize(320*2,480*2);
+    
+    var root = new amino.ProtoGroup();
+    root.setScalex(2).setScaley(2);
+    stage.setRoot(root);
+    
+    var lv = new widgets.ListView().setW(320).setH(480);
+    root.add(lv);
+});

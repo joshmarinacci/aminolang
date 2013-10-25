ffa706a87806f1c8e32f5859e64429223c76661d
Josh Marinacci <joshua@marinacci.org>
2013-10-24 21:47:05 -0500

    added dockable tabs

--- tests/examples/finder.js
+++ tests/examples/finder.js
@@ -38,6 +38,8 @@ for(var i=0; i<3; i++) {
 
 
 
+var windowlist = [];
+
 
 IconView = amino.ComposeObject({
     type:"IconView",
@@ -77,12 +79,10 @@ IconView = amino.ComposeObject({
 
 var root = null;
 function openView(item) {
-    console.log("opening a view for the item: ", item);
     console.log("folder = " + item.isFolder());
     if(item.isFolder()) {
         var folder = item;
-        var view = new WindowView();
-        view.comps.title.setText(folder.getTitle());
+        var view = new ContentView();
         
         if(folder.customizer) {
             console.log("doing custom version");
@@ -114,6 +114,7 @@ function openView(item) {
                     lv.setModel(item.getItems());
                 });
             }
+            
             view.comps.toolbar
                 .add(new widgets.ToggleButton()
                     .setW(30).setH(20)
@@ -123,48 +124,212 @@ function openView(item) {
                     .setW(30).setH(20)
                     .setText("list").setFontSize(10)
                     )
-                .add(new widgets.PushButton()
-                    .setW(30).setH(20)
-                    .setText("close").setFontSize(10)
-                    .onAction(function(e) {
-                        view.setVisible(false);
-                    })
-                    )
                 ;
+                
             view.comps.toolbar.redoLayout();
+            
         }
         
         view.setFill("#ff0000");
         view.setW(400).setH(300);
         view.setTx(300).setTy(200);
-        if(folder.windowx) view.setTx(folder.windowx);
-        if(folder.windowy) view.setTy(folder.windowy);
-        if(folder.windoww) view.setW(folder.windoww);
-        if(folder.windowh) view.setH(folder.windowh);
-            
-        root.add(view);
+          
+        var winview = new WindowView();
+        winview.addTab(view,folder.getTitle());
+        root.add(winview);
+        
+        /*
+        if(folder.windowx) winview.setTx(folder.windowx);
+        if(folder.windowy) winview.setTy(folder.windowy);
+        if(folder.windoww) winview.setW(folder.windoww);
+        if(folder.windowh) winview.setH(folder.windowh);
+        */
     } else {
         var view = new WindowView();
-        view.comps.title.setText(item.getTitle());
         view.setFill("#ffffff");
-        view.comps.toolbar
-            .add(new widgets.PushButton()
-                .setW(30).setH(20)
-                .setText("close").setFontSize(10)
-                .onAction(function(e) {
-                    view.setVisible(false);
-                })
-                );
-
         var text = new widgets.TextField()
             .setText(item.doc.content);
         view.comps.contents.add(text);
-        view.setW(400).setH(300);
-        view.setTx(300).setTy(200);
-        root.add(view);
+        var winview = new WindowView();
+        winview.addTab(view,item.getTitle());
+        root.add(winview);
     }
+    
+    winview.setTx(100).setTy(100).setW(500).setH(300);
 }
 
+ContentView = amino.ComposeObject({
+    type:"ContentView",
+    extend: amino.ProtoWidget,
+    comps: {
+        background: {
+            proto: amino.ProtoRect,
+            promote: ['fill','w','h'],
+        },
+        contents: {
+            proto: amino.ProtoGroup,
+        },
+        toolbar: {
+            proto: widgets.HorizontalPanel,
+        },
+    },
+    props: {
+        w: {
+            value: 300,
+            set: function(w) {
+                this.props.w = w;
+                this.comps.background.setW(w);
+                this.comps.toolbar.setW(w);
+                this.comps.contents.children.forEach(function(ch) {
+                    if(ch.setW) ch.setW(w);
+                    if(ch.setTx) ch.setTx(0);
+                });
+                return this;
+            }
+        },
+        h: {
+            value: 300,
+            set: function(h) {
+                this.props.h = h;
+                this.comps.background.setH(h);
+                this.comps.toolbar.setH(h);
+                this.comps.contents.children.forEach(function(ch) {
+                    if(ch.setH) ch.setH(h);
+                    if(ch.setTy) ch.setTy(0);
+                });
+                return this;
+            }
+        },
+    },
+    init: function() {
+        this.comps.base.add(this.comps.background);
+        this.comps.base.add(this.comps.contents);
+    },
+    
+});
+
+function splitWindow(tab, window,x,y) {
+    window.comps.contents.remove(tab.content);
+    window.comps.tabholder.remove(tab);
+    window.layoutTabs();
+    var winview = new WindowView();
+    winview.setTx(x).setTy(y).setW(window.getW()).setH(window.getH());
+    winview.addExistingTab(tab);
+    root.add(winview);
+}
+
+function mergeWindow(tab, window, gpt) {
+    tab.window.comps.contents.remove(tab.content);
+    tab.window.comps.tabholder.remove(tab);
+    tab.window.layoutTabs();
+    if(tab.window.getTabCount() < 1) {
+        tab.window.destroy();
+    }
+    tab.window = window;
+    tab.window.addExistingTab(tab);
+    root.raiseToTop(tab.window);
+}
+
+TabView = amino.ComposeObject({
+    type: "Tabview",
+    extend: amino.ProtoWidget,
+    comps: {
+        background: {
+            proto: amino.ProtoRect,
+            promote: ['fill'],
+        },
+        title: {
+            proto: widgets.Label,
+            promote: ['text'],
+        },
+        closeButton: {
+            proto: widgets.PushButton,
+        },
+    },
+    props: {
+        w: {
+            value: 100,
+            set: function(w) {
+                this.props.w = w;
+                this.comps.background.setW(w);
+                return this;
+            }
+        },
+        h: {
+            value: 30,
+            set: function(h) {
+                this.props.h = h;
+                this.comps.background.setH(h);
+                return this;
+            }
+        },
+    },
+    init: function() {
+        this.comps.base.add(this.comps.background);
+        this.comps.base.add(this.comps.title);
+        this.comps.base.add(this.comps.closeButton);
+        this.comps.background.setW(100);
+        this.comps.background.setH(30);
+        this.comps.closeButton.setW(40).setText("x").setH(20).setTx(100);
+        this.comps.title.setText("foo").setTx(0);
+        this.setW(140).setH(30);
+        var self = this;
+        var ty = 0;
+        var broken = false;
+        this.children = [
+            this.comps.closeButton,
+        ];
+        
+        amino.getCore().on("action",this.comps.closeButton, function(e) {
+                if(self.window.getTabCount() == 1) {
+                    console.log("closing window");
+                    self.window.destroy();
+                } else {
+                    console.log("closing tab");
+                    self.window.removeTab(self);
+                }
+        });
+        amino.getCore().on("press",this,function(e) {
+            self.window.raiseContentToFront(self.content);
+            root.raiseToTop(self.window);
+        });
+        amino.getCore().on("drag",this, function(e) {
+            if(broken || self.window.getTabCount() == 1) {
+                //drag the whole window
+                self.window.setTx(self.window.getTx()+e.dx);
+                self.window.setTy(self.window.getTy()+e.dy);
+                
+                var pt = amino.getCore().localToGlobal(e.point,e.target);
+                windowlist.forEach(function(win) {
+                    var tb = win.comps.titlebar;
+                    var lpt = amino.getCore().globalToLocal(pt,tb);
+                    if(tb.contains(lpt.x,lpt.y) && tb != self.window.comps.titlebar) {
+                        mergeWindow(self,win,pt);
+                        broken = false;
+                    }
+                });
+                return;
+            } else {
+                //just drag the tab
+                self.setTx(self.getTx()+e.dx);
+                ty += e.dy;
+            }
+            if(ty < -20 || ty > self.getH()+20) {
+                if(self.window.getTabCount() > 1) {
+                    var pt = e.point;
+                    pt = amino.getCore().localToGlobal(pt,e.target);
+                    splitWindow(self,self.window,pt.x,pt.y);
+                    broken = true;
+                }
+            }
+        });
+        amino.getCore().on("release",this,function(e) {
+            ty = 0;
+            broken = false;
+            self.window.layoutTabs();
+        });
+    },
+});
 WindowView = amino.ComposeObject({
     type:"WindowView",
     extend: amino.ProtoWidget,
@@ -179,11 +344,8 @@ WindowView = amino.ComposeObject({
         titlebar: {
             proto: amino.ProtoRect,
         },
-        toolbar: {
-            proto: widgets.HorizontalPanel,
-        },
-        title: {
-            proto: widgets.Label,
+        tabholder: {
+            proto: amino.ProtoGroup,
         },
         grabber: {
             proto: amino.ProtoRect,
@@ -196,11 +358,14 @@ WindowView = amino.ComposeObject({
                 this.props.w = w;
                 this.comps.background.setW(w);
                 this.comps.titlebar.setW(w);
-                this.comps.title.setW(w);
-                this.comps.toolbar.setW(w);
+                this.comps.tabholder.setW(w);
+                //this.comps.title.setW(w);
+                //this.comps.toolbar.setW(w);
                 this.comps.grabber.setTx(w-this.comps.grabber.getW());
+                
                 this.comps.contents.children.forEach(function(ch) {
                     if(ch.setW) ch.setW(w);
+                    if(ch.setTx) ch.setTx(0);
                 });
                 return this;
             }
@@ -211,12 +376,15 @@ WindowView = amino.ComposeObject({
                 this.props.h = h;
                 this.comps.background.setH(h);
                 this.comps.contents.setH(h-30);
+                this.comps.tabholder.setH(30);
                 var g = this.comps.grabber;
                 g.setTy(h-g.getH());
+                
                 this.comps.contents.children.forEach(function(ch) {
                     if(ch.setH) ch.setH(h-30);
                     if(ch.setTy) ch.setTy(0);
                 });
+                
                 return this;
             }
         },
@@ -239,16 +407,14 @@ WindowView = amino.ComposeObject({
         this.comps.base.add(this.comps.contents);
         this.comps.base.add(this.comps.grabber);
         this.comps.base.add(this.comps.titlebar);
-        this.comps.base.add(this.comps.toolbar);
-        this.comps.base.add(this.comps.title);
+        this.comps.base.add(this.comps.tabholder);
         
         this.comps.background.setFill("#eeeeee");
         this.comps.contents.setTy(30);
         
-        this.comps.titlebar.setW(100).setH(30).setFill("#cccccc");
-        this.comps.toolbar.setW(100).setH(30).setGap(2).setPadding(2);
-        this.comps.title.setAlign("center").setTy(3);
+        this.comps.titlebar.setW(100).setH(30).setFill("#ccccff");
         this.comps.grabber.setW(30).setH(30).setFill("#555555");
+        
         var self = this;
         amino.getCore().on("drag",this.comps.grabber,function(e) {
             self.setW(self.getW()+e.dx);
@@ -271,10 +437,62 @@ WindowView = amino.ComposeObject({
             this.comps.background,
             this.comps.contents, 
             this.comps.titlebar,
-            this.comps.toolbar, 
+            this.comps.tabholder, 
             this.comps.grabber,
-            
         ];
+        
+        this.addTab = function(view, title) {
+            this.comps.contents.add(view);
+            var count = this.comps.tabholder.children.length;
+            var tab = new TabView().setText(title).setTx(count*150);
+            tab.window = this;
+            tab.content = view;
+            this.comps.tabholder.add(tab);
+            tab.setTx(count*150);
+        };
+        
+        this.addExistingTab = function(tab) {
+            this.comps.contents.add(tab.content);
+            var count = this.comps.tabholder.children.length;
+            tab.window = this;
+            this.comps.tabholder.add(tab);
+            tab.setTx(count*150);
+        }
+        
+        this.layoutTabs = function() {
+            //re-sort them by tx
+            var arr = [];
+            this.comps.tabholder.children.forEach(function(tab) {
+                arr[tab.getTx()] = tab;
+            });
+            //layout again in the new order
+            var tx = 0;
+            arr.forEach(function(tab) {
+                tab.setTx(tx);
+                tx+= 150;
+            });
+        }
+        
+        this.getTabCount = function() {
+            return this.comps.tabholder.children.length;
+        }
+        windowlist.push(this);        
+        
+        this.destroy = function() {
+            root.remove(this);
+            var n = windowlist.indexOf(this);
+            windowlist.splice(n,1);
+        }
+        
+        this.removeTab = function(tab) {
+            this.comps.contents.remove(tab.content);
+            this.comps.tabholder.remove(tab);
+            this.layoutTabs();
+        }
+        
+        this.raiseContentToFront = function(content) {
+            this.comps.contents.raiseToTop(content);
+        }
     }
 });
 
@@ -518,7 +736,7 @@ amino.startApp(function(core, stage) {
                 .setItem(item)
         );
     }
-    desktopview.comps.title.setText("desktop");
+    //desktopview.comps.title.setText("desktop");
     root.add(desktopview);
     
     

c96ed2e02fae6078d57c2fd8a6fe4dc3553c2a73
Joshua Marinacci <joshua.marinacci@nokia.com>
2014-07-18 12:31:24 -0700

    added 3d globe demo

--- src/sg/amino.js
+++ src/sg/amino.js
@@ -1,4 +1,4 @@
-/** 
+/**
 @class dummy
 @desc a dummy header to work around a doc generation bug. ignore
 */
@@ -7,7 +7,7 @@ if(typeof document == "undefined") {
     exports.sgtest = require('./aminonative.node');
     var input = require('aminoinput');
 } else {
-    var exports = this['amino'] = {};    
+    var exports = this['amino'] = {};
     exports.inbrowser = true;
     var input = this['aminoinput'];
 }
@@ -83,7 +83,7 @@ var defaultFonts = {
                 normal: "SourceSansPro-Regular.ttf",
                 italic: "SourceSansPro-Italic.ttf",
             },
-            
+
             600: {
                 normal: "SourceSansPro-Semibold.ttf",
                 italic: "SourceSansPro-SemiboldItalic.ttf",
@@ -142,7 +142,7 @@ exports.native = {
     },
     updateProperty: function(handle, name, value) {
         propertyCount++;
-        exports.sgtest.updateProperty(handle, propsHash[name], value);        
+        exports.sgtest.updateProperty(handle, propsHash[name], value);
     },
     setRoot: function(handle) {
         exports.sgtest.addNodeToGroup(handle,this.rootWrapper);
@@ -193,11 +193,11 @@ exports.native = {
     updateAnimProperty: function(handle, prop, type) {
         exports.sgtest.updateAnimProperty(handle, propsHash[prop], type);
     },
-    
+
     createPropAnim: function(node,prop,start,end,dur) {
         return new SGAnim(node,prop,start,end,dur);
     },
-    
+
     runTest: function(opts) {
         return exports.sgtest.runTest(opts);
     },
@@ -253,7 +253,7 @@ bus.filter(typeIs("animend"))
     .onValue(core.notifyAnimEnd);
     */
 
-/** 
+/**
 @func ComposeObject transform the supplied prototype object into a constructor that can then be invoked with 'new'
 */
 exports.ComposeObject = function(proto) {
@@ -272,7 +272,7 @@ exports.ComposeObject = function(proto) {
             });
         }
     }
-    
+
     function delegateProp(obj, name, prop) {
         obj.props[name] = prop.value;
         obj["set"+camelize(name)] = function(value) {
@@ -287,7 +287,7 @@ exports.ComposeObject = function(proto) {
             obj["set"+camelize(name)] = prop.set;
         }
     }
-    
+
     function generalizeProp(obj, name, prop) {
         obj["set"+camelize(name)] = function(value) {
             obj.set(name,value);
@@ -296,7 +296,7 @@ exports.ComposeObject = function(proto) {
     }
     return function() {
         var obj = this;
-        
+
         if(proto.extend) {
             var sup = new proto.extend();
             for(var name in sup) {
@@ -306,7 +306,7 @@ exports.ComposeObject = function(proto) {
             obj.props = {};
             obj.comps = {};
         }
-        
+
         if(proto.comps) {
             for(var name in proto.comps) {
                 delgate(obj,name,proto.comps[name]);
@@ -317,7 +317,7 @@ exports.ComposeObject = function(proto) {
                 delegateProp(obj, name, proto.props[name]);
             }
         }
-        
+
         if(proto.set) {
             obj.set = proto.set;
             for(var name in proto.props) {
@@ -343,7 +343,7 @@ exports.ComposeObject = function(proto) {
 
 
 var propsHash = {
-    
+
     //general
     "visible":18,
     "opacity":27,
@@ -355,7 +355,7 @@ var propsHash = {
     "h":11,
     "x":21,
     "y":22,
-    
+
     //transforms
     "tx":23,
     "ty":1,
@@ -364,12 +364,12 @@ var propsHash = {
     "rotateZ":4,
     "rotateX":19,
     "rotateY":20,
-    
+
     //text
     "text":9,
     "fontSize":12,
     "fontId":28,
-    
+
     //animation
     "count":29,
     "lerplinear":13,
@@ -378,24 +378,24 @@ var propsHash = {
     "lerpprop":16,
     "lerpcubicinout":17,
     "autoreverse":35,
-    
-    
+
+
     //geometry
     "geometry":24,
     "filled":25,
     "closed":26,
     "dimension": 36,
-    
+
     //rectangle texture
     "textureLeft":  30,
     "textureRight": 31,
     "textureTop":   32,
-    "textureBottom":33,    
-    
+    "textureBottom":33,
+
     //clipping
     "cliprect": 34,
-    
-   
+
+
 }
 
 exports.propsHash = propsHash;
@@ -445,7 +445,7 @@ exports.ProtoRect = exports.ComposeObject({
         opacity: { value: 1},
         /** @prop fill fill color of the rectangle. Should be a hex value like #af03b6 */
         fill: {
-            value: '#ff0000', 
+            value: '#ff0000',
         }
     },
     //replaces all setters
@@ -460,7 +460,7 @@ exports.ProtoRect = exports.ComposeObject({
                 exports.native.updateProperty(this.handle,name,value);
             }
         }
-        
+
         if(name == 'fill') {
             var color = ParseRGBString(value);
             this.setR(color.r);
@@ -486,8 +486,8 @@ exports.ProtoRect = exports.ComposeObject({
         this.shortCircuit = true;
         this.type = "rect";
         var rect = this;
-        
-        /** @func contains  returns true if the rect contains the x and y. 
+
+        /** @func contains  returns true if the rect contains the x and y.
         x and y should be in the coordinate space of the rectangle.
         */
         this.contains = function(x,y) {
@@ -498,7 +498,7 @@ exports.ProtoRect = exports.ComposeObject({
             }
             return false;
         }
-        
+
     }
 });
 
@@ -565,7 +565,7 @@ exports.ProtoPoly = exports.ComposeObject({
         dimension: { value: 2 },
         /** @prop fill fill color of the polygon, if filled. Should be a hex value like #af03b6 */
         fill: {
-            value: '#ff0000', 
+            value: '#ff0000',
         },
         opacity: { value: 1},
         filled: { value: 0 },
@@ -578,7 +578,7 @@ exports.ProtoPoly = exports.ComposeObject({
         if(name == 'visible') value = (value?1:0);
         if(name == 'filled')  value = (value?1:0);
         if(name == 'closed')  value = (value?1:0);
-        
+
         if(shortCircuit(this, this.props[name],value)) return;
         this.dirty = true;
         this.props[name] = value;
@@ -588,7 +588,7 @@ exports.ProtoPoly = exports.ComposeObject({
                 exports.native.updateProperty(this.handle,name,value);
             }
         }
-        
+
         if(name == 'fill') {
             var color = ParseRGBString(value);
             this.setR(color.r);
@@ -636,6 +636,9 @@ exports.ProtoGroup = exports.ComposeObject({
         scalex: { value: 1 },
         /** @prop scaley scale Y. @default 1*/
         scaley: { value: 1 },
+        rotateX: { value: 0},
+        rotateY: { value: 0},
+        rotateZ: { value: 0},
         /** @prop visible visible or not. 1 or 0, not true or false. @default 1 */
         visible: { value: 1 },
         w: { value: 100 },
@@ -670,7 +673,7 @@ exports.ProtoGroup = exports.ComposeObject({
             return this;
         }
         this.isParent = function() { return true; }
-        
+
         /** @func getChildCount()  returns the number of child nodes inside this group */
         this.getChildCount = function() {
             return this.children.length;
@@ -699,7 +702,7 @@ exports.ProtoGroup = exports.ComposeObject({
         }
         this.shortCircuit = true;
         this.type = "group";
-        
+
         this.raiseToTop = function(node) {
             if(node == undefined) abort("can't move a null child");
             if(!this.live) abort("error. can't move a child in a group that isn't live yet");
@@ -711,7 +714,7 @@ exports.ProtoGroup = exports.ComposeObject({
     }
 });
 
-/** 
+/**
 @class ProtoText
 @desc  The text primitive. Use it to draw a single run of text with the
 same font, size, and color.
@@ -741,7 +744,7 @@ exports.ProtoText = exports.ComposeObject({
         b: { value: 0},
         /** @prop fill fill color of the rectangle. Should be a hex value like #af03b6 */
         fill: {
-            value: '#000000', 
+            value: '#000000',
         },
         id: { value: 'no id' },
     },
@@ -768,7 +771,7 @@ exports.ProtoText = exports.ComposeObject({
             }
             exports.native.updateProperty(this.handle, name, value);
         }
-                
+
         if(name == 'fill') {
             var color = ParseRGBString(value);
             this.setR(color.r);
@@ -804,7 +807,7 @@ exports.ProtoText = exports.ComposeObject({
     }
 });
 
-/** 
+/**
 @class ImageView
 @desc a view which shows an image loaded from a path. It will always show the full image.
 */
@@ -834,10 +837,10 @@ exports.ProtoImageView = exports.ComposeObject({
         /** @prop visible @default true */
         visible: { value:1 },
         /** @prop src  the file to load this image from @default null */
-        src: { 
+        src: {
             value: null ,
         },
-        
+
         textureLeft:   { value: 0 },
         textureRight:  { value: 1 },
         textureTop:    { value: 0 },
@@ -875,7 +878,7 @@ exports.ProtoImageView = exports.ComposeObject({
                     }
                 });
             }
-            
+
         }
     },
     init: function() {
@@ -885,7 +888,7 @@ exports.ProtoImageView = exports.ComposeObject({
         for(var propname in this.props) {
             this.set(propname, this.props[propname]);
         }
-        /** @func contains  returns true if the rect contains the x and y. 
+        /** @func contains  returns true if the rect contains the x and y.
         x and y should be in the coordinate space of the rectangle.
         */
         var rect = this;
@@ -905,7 +908,7 @@ exports.ProtoImageView = exports.ComposeObject({
 @class ProtoWidget
 @desc  the base class for all widgets. Widgets are a kind of node used for UI controls. All widgets
 have a size (width/height), can be hidden (visible = false), an ID, and left/right/top/bottom properties
-to be used by layout panels. 
+to be used by layout panels.
 */
 exports.ProtoWidget = exports.ComposeObject({
     type: "widget",
@@ -965,7 +968,7 @@ exports.ProtoWidget = exports.ComposeObject({
 
 /** @class Stage
 @desc A stage represents a window. On mobile devices there will only be one stage. On desktop there can be multiple. A stage
-can only be created by using the core.createStage() function. 
+can only be created by using the core.createStage() function.
 */
 function SGStage(core) {
 	this.core = core;
@@ -981,7 +984,7 @@ function SGStage(core) {
 	this.getH = function() {
 	    return exports.native.getWindowSize().h;
 	}
-	/** @func on(name,node,cb) sets a callback for events matching the specified name on the 
+	/** @func on(name,node,cb) sets a callback for events matching the specified name on the
 	specified node. Use null for the node to match global events. */
 	this.on = function(name, node, cb) {
 		this.core.on(name, node, cb);
@@ -1035,7 +1038,7 @@ function JSFont(desc) {
         this.weights[weight] = exports.native.createNativeFont(aminodir+"/fonts/"+desc.weights[weight].normal);
     }
     process.chdir(dir);
-    
+
     this.getNative = function(size, weight, style) {
         if(this.weights[weight] != undefined) {
             return this.weights[weight];
@@ -1084,7 +1087,7 @@ function SGAnim(node, prop, start, end, dur) {
             this.prop,
             this.start,this.end,this.duration);
     }
-    /** @func setIterpolator(type) Sets the interpolator to use for this animation. Valid values include: 
+    /** @func setIterpolator(type) Sets the interpolator to use for this animation. Valid values include:
       amino.Interpolators.CubicIn and amino.Interpolators.CubicInOut and amino.Interpolators.Linear
      */
     this.setInterpolator = function(lerptype) {
@@ -1117,7 +1120,7 @@ function SGAnim(node, prop, start, end, dur) {
         this.afterCallbacks.push(cb);
         return this;
     }
-    
+
     this.before = function(cb) {
         this.beforeCallbacks.push(cb);
         return this;
@@ -1126,13 +1129,13 @@ function SGAnim(node, prop, start, end, dur) {
 
 
 
-/** 
+/**
 @class Core
-@desc The core of Amino. Only one will exist at runtime. Always access through the callback 
+@desc The core of Amino. Only one will exist at runtime. Always access through the callback
 */
 function Core() {
     this.anims = [];
-    /** @func createPropAnim(node, propertyName, startValue, endValue, duration, count, autoreverse) 
+    /** @func createPropAnim(node, propertyName, startValue, endValue, duration, count, autoreverse)
     creates a new property animation. Node is the node to be animated. propertyName is the string name of the property
     to animate. This should be a numeric property like tx or scalex. start and end are the starting and ending values
     of the animation. Duration is the length of the animation in milliseconds. 1000 = one second. Count is
@@ -1157,7 +1160,7 @@ function Core() {
             }
         }
     }
-    
+
     var self = this;
     this.init = function() {
         exports.native.init(this);
@@ -1173,7 +1176,7 @@ function Core() {
             input.processEvent(self,e);
         });
     }
-    
+
     this.root = null;
     this.start = function() {
         var core = this;
@@ -1189,7 +1192,7 @@ function Core() {
         if(!this.root) {
             throw new Error("ERROR. No root set on stage");
         }
-        
+
         var self = this;
         function immediateLoop() {
             try {
@@ -1212,18 +1215,18 @@ function Core() {
         }
         setTimeout(immediateLoop,1);
     }
-    
+
     /** @func createStage(w,h)  creates a new stage. Only applies on desktop. */
     this.createStage = function(w,h) {
         exports.native.createWindow(this,w,h);
         this.stage = new SGStage(this);
         return this.stage;
     }
-    
+
     this.getFont = function(name) {
         return exports.native.getFont(name);
     }
-    
+
     this.setRoot = function(node) {
         exports.native.setRoot(node.handle);
         this.root = node;
@@ -1236,17 +1239,17 @@ function Core() {
     }
     this.findNodeAtXY_helper = function(root,x,y,tab) {
         if(!root) return null;
-        //console.log(tab + 
-        //    (root.getId?root.getId():"-") + " " + root.getTx() + " " + root.getTy() + " " 
+        //console.log(tab +
+        //    (root.getId?root.getId():"-") + " " + root.getTx() + " " + root.getTy() + " "
         //    + (root.getW?root.getW():"-") + " x " + (root.getH?root.getH():"-"));
         if(!root.getVisible()) return null;
-        
+
         var tx = x-root.getTx();
         var ty = y-root.getTy();
         tx = tx/root.getScalex();
         ty = ty/root.getScaley();
         //console.log(tab + "   xy="+tx+","+ty);
-    
+
         if(root.children) {
             for(var i=root.children.length-1; i>=0; i--) {
                 var node = root.children[i];
@@ -1277,7 +1280,7 @@ function Core() {
     this.globalToLocal = function(pt, node) {
         return this.globalToLocal_helper(pt,node);
     }
-    
+
     this.globalToLocal_helper = function(pt, node) {
     	if(node.parent) {
     		pt =  this.globalToLocal_helper(pt,node.parent);
@@ -1325,7 +1328,7 @@ function Core() {
     }
     this.fireEvent = function(event) {
         if(!event.type) { console.log("WARNING. Event has no type!"); }
-        
+
        // var t1 = process.hrtime();
         if(this.listeners[event.type]) {
             var arr = this.listeners[event.type];
@@ -1341,7 +1344,7 @@ function Core() {
             //console.log('validate time = ',process.hrtime(t1)[1]/1e6);
         }
     };
-    
+
     this.requestFocus = function(target) {
         if(this.keyfocus) {
             this.fireEventAtTarget(this.keyfocus,{type:"focusloss",target:this.keyfocus});
@@ -1351,7 +1354,7 @@ function Core() {
             this.fireEventAtTarget(this.keyfocus,{type:"focusgain",target:this.keyfocus});
         }
     }
-    
+
     this.runTest = function(opts) {
         console.log("running the test with options",opts);
         return exports.native.runTest(opts);
@@ -1395,5 +1398,3 @@ exports.Interpolators = {
 exports.setHiDPIScale = function(scale) {
     Core.DPIScale = scale;
 }
-
-
---
+++ tests/3d/globe.js
@@ -0,0 +1,75 @@
+var amino = require('amino.js');
+var widgets= require('widgets.js');
+
+var data = require('./countries.js');
+
+amino.startApp(function(core, stage) {
+
+    // set up the stage
+    stage.setSize(1280,720);
+    var group = new amino.ProtoGroup();
+    stage.setRoot(group);
+
+
+    var cos = Math.cos;
+    var sin = Math.sin;
+    var PI = Math.PI;
+
+    function latlon2xyz(lat,lon, rad) {
+        var el = lat/180.0*PI;
+        var az = lon/180.0*PI;
+        var x = rad * cos(el) * sin(az);
+        var y = rad * cos(el) * cos(az);
+        var z = rad * sin(el);
+        return [x,y,z];
+    }
+
+    function addCountry(nz) {
+        //make the geometry
+        for(var i=0; i<nz.borders.length; i++) {
+            var border = nz.borders[i];
+            var points = [];
+            var poly = new amino.ProtoPoly();
+            for(var j=0; j<border.length; j++) {
+                var point = border[j];
+                var pts = latlon2xyz(point.lat,point.lng,200);
+                points.push(pts[0]);
+                points.push(pts[1]);
+                points.push(pts[2]);
+            }
+            poly.setFill("#80ff80");
+            poly.setGeometry(points);
+            poly.setDimension(3);
+            group.add(poly);
+        }
+    }
+
+    for(var i=0; i<data.countries.length; i++) {
+        addCountry(data.countries[i]);
+    }
+
+    function addLine(lat,lon,rad) {
+        var poly = new amino.ProtoPoly();
+        var pt1 = latlon2xyz(lat,lon,200);
+        var pt2 = latlon2xyz(lat,lon,rad);
+        var points = pt1.concat(pt2);
+        poly.setFill("#ff0000");
+        poly.setGeometry(points);
+        poly.setDimension(3);
+        group.add(poly);
+    }
+    
+    //add a line at portland
+    addLine(45.52, -122.681944, 300);
+
+    // center
+    group.setTx(1280/2).setTy(720/2);
+    //turn earth upright
+    group.setRotateX(90);
+    group.setRotateY(0);
+    group.setRotateZ(0);
+
+    // spin it forever
+    core.createPropAnim(group,"rotateZ",0,-360,30*1000).setCount(-1);
+
+});
